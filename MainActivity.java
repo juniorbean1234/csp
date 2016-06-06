9cfc214d2f7b21bfd0e2d13a04a88fa050a123a5
DmCDante0609 <runryan22d@gmail.com>
2016-06-06 09:20:29 -0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lor.mywebviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    Button btnBack;
+    WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview=(WebView)findViewById(R.id.webview1);
+        webview.setWebViewClient(new MyWebViewClient());
+        openURL();
+    }
+
+    /** Opens the URL in a browser */
+    private void openURL() {
+        webview.loadUrl("http://www.bsd405.org");
+        webview.requestFocus();
+    }
+}
